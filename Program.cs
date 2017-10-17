37f018ff7dae93e20d82d7eb6271f9f3458a90cb
Patrick <kaorti@gmail.com>
2017-10-17 11:24:07 -0700

    human class exercise

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace human
+{
+    public class Human {
+        public String name;
+        public int strength = 3;
+        public int dexterity = 3;
+        public int intellegence = 3;
+        public int health = 100;
+
+        public Human(String initName){
+            name = initName;
+        }
+        public Human(String initName, int initStrength, int initDexterity, int initIntellegence, int initHealth){
+            name = initName;
+            strength = initStrength;
+            dexterity = initDexterity;
+            intellegence = initIntellegence;
+            health = initHealth;
+        }
+        public void Attack(Human victim){
+            if (victim is Human)
+            {
+                victim.health -= 5*strength;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human bob = new Human("bob");
+            Human fred = new Human("fred");
+            bob.Attack(fred);
+        }
+    }
+}
